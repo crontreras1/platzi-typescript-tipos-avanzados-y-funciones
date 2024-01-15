135b1e953818ffe584511a8ea4001099a8a6b9d6
crontreras1 <crontreras1@gmail.com>
2024-01-15 16:57:16 -0500

    class 16. CRUD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,26 @@
 // Ejecución
-import { addProduct } from './products/product.service'
+import {faker} from '@faker-js/faker'
+import { addProduct, products } from './products/product.service'
 
 addProduct({
-    id: '1',
-    title: 'product1',
-    createdAt: new Date(),
-    updateAt: new Date(),
-    stock: 23,
+    id: faker.datatype.uuid(),
+    image: faker.image.imageUrl(),
+    description: faker.commerce.productDescription(),
+    title: faker.commerce.productName(),
+    createdAt: faker.date.recent(),
+    updateAt: faker.date.recent(),
+    stock: faker.datatype.number({min: 10, max: 100}),
+    size: faker.helpers.arrayElement(['S', 'L', 'M', 'XL']),
+    color: faker.color.cssSupportedFunction(),
+    price: parseInt(faker.commerce.price(), 10),
     category: {
-        id: '1',
-        name: 'c1',
-        createdAt: new Date(),
-        updateAt: new Date()
-    }
+        id: faker.datatype.uuid(),
+        name: faker.commerce.department(),
+        createdAt: faker.date.recent(),
+        updateAt: faker.date.recent()
+    },
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements([]),
 })
+
+console.log(products)
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -6,3 +6,19 @@ export const products: Product[] = []
 export const addProduct = (data: Product) => {
     products.push(data)
 }
+
+// export const updateProduct = (id: string, changes: Product) => {
+
+// }
+
+// export const deleteProduct = (id: string, changes: Product) => {
+
+// }
+
+// export const getProduct = (id: string, changes: Product) => {
+
+// }
+
+// export const searchProduct = (id: string, changes: Product) => {
+
+// }
